c93ae74fd16e41b154b17bca88cbf5107ee45ff7
MohammadSheakh <mohammad.sheakh@gmail.com>
2022-12-18 20:11:30 +0600

    Array and This

--- learn-mern-like-a-pro/function.js
+++ learn-mern-like-a-pro/function.js
@@ -1,3 +1,4 @@
+// lecture ---------------------------------------- 9
 console.log("function started ");
 
 var m = { x: 10 };
@@ -18,3 +19,33 @@ function change2(n) {
 
 change2(m);
 console.log("value of m  outside function: ", m);
+
+//////////////////////////////////////////////////
+function inputType({ name }) {
+    // receiver
+    console.log("print name : ", name);
+}
+
+inputType({ name: "hello" }); // sender
+console.log("=------------------------------------------");
+
+function inputType2({ name, age, ...rest }) {
+    // receiver // rest mane baki shob kichu // baki shob parameter .. she ekta array baniye rest er moddhe
+    // set kore dibe ..
+    console.log("print name : ", name);
+    console.log("print age : ", age);
+    console.log("print rest : ", rest);
+    console.log("..................................................");
+    console.log("arguments", arguments);
+}
+
+inputType2({
+    name: "hello",
+    age: "3",
+    subject1: "bangla",
+    subject2: "english",
+}); // sender
+
+console.log("--------------------------------------------------");
+
+// arguments hocche built in types ..
